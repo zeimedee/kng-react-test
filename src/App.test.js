535075f19e2d84d6e5e58a6e28c4500cda161906
alexander <zeimedee@gmail.com>
2021-05-12 20:47:28 +0000

    :rocket: test code

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,38 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import {render, unmountComponentAtNode} from "react-dom";
+import {act} from "react-dom/test-utils"
+import Radio from './radio'
+
+let container = null;
+
+beforeEach(()=>{
+  container = document.createElement("div");
+  document.body.appendChild(container);
 });
+
+afterEach(()=>{
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it("changes color when clicked", ()=>{
+    act(()=>{
+      render(<Radio />, container)
+    });
+    expect(container.textContent).toBe("text");
+    const radio1 = document.querySelector("#rad1");
+    const radio2 = document.querySelector("#rad2");
+
+    act(()=>{
+      radio1.dispatchEvent(new MouseEvent("click", { bubbles: true}));
+    });
+    expect(container).toMatchSnapshot();
+    act(()=>{
+      radio2.dispatchEvent(new MouseEvent("Click", {bubbles: true}));
+    })
+    expect(container).toMatchSnapshot();
+
+
+    
+})
---
+++ src/radio.js
@@ -0,0 +1,34 @@
+import React,{useState} from 'react'
+
+function Radio() {
+    const [color,setColor] = useState(true)
+    const [state,setState] = useState(null)
+    return (
+        <div>
+                <h1 id="text" style={{color: color ? 'red': 'green'}}>text</h1> 
+                <input 
+                    id="rad1"
+                    name="b1"
+                    type="radio"
+                    onChange={()=>{
+                        setColor(!color)
+                        setState(false)
+                    }}
+                    checked={!state}
+                    />
+                    <input 
+                    name="b2"
+                    id="rad2"
+                    type="radio"
+                    onChange={()=>{
+                        setColor(!color)
+                        setState(true)
+                    }}
+                    checked={state}
+                    />
+        </div>
+        
+    )
+}
+
+export default Radio
